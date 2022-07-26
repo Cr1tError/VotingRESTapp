cfb1edc3be507e04a4ed13fad7dee49c454ac18a
Cr1tError <maksym.kokora@gmail.com>
2022-07-26 10:02:56 +0200

    init

---
+++ src/main/java/org/mk/VotingRESTapp/VotingResTappApplication.java
@@ -0,0 +1,13 @@
+package org.mk.VotingRESTapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingResTappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingResTappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mk/VotingRESTapp/VotingResTappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mk.VotingRESTapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingResTappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
